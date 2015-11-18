5f6fdf674592adc730895b4d572e2d05f3fd7db4
Dyaa Eldin <mail@dyaa.me>
2015-11-18 20:32:05 +0200

    Adding documentation2

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
 	name: 'dyaa:digitalocean',
 	version: '0.1.1_3',
-	// Brief, one-line summary of the package.
 	summary: 'DigitalOcean API V2 wrapper',
-	// URL to the Git repository containing the source code for this package.
 	git: 'https://github.com/dyaa/meteor-digitalocean',
-	// By default, Meteor will default to using README.md for documentation.
-	// To avoid submitting documentation, set this field to null.
 	documentation: 'README.md'
 });
 
